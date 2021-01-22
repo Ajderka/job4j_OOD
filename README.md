a6fb95fd324c9250b0a58beb4e1a44c7597836a6
Айдер <emage.haf@gmail.com>
2021-01-22 23:06:02 +0200

    Second commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+
 # Level Junior path 4. Like OOD.
---
+++ src/main/java/ru/job4j/ood/SimpleSequenceGenerator.java
@@ -0,0 +1,17 @@
+package ru.job4j.ood;
+
+import java.util.List;
+import java.util.Random;
+
+public class SimpleSequenceGenerator implements SequenceGenerator<Integer> {
+    @Override
+    public List<Integer> generate(int size) {
+        Random random = new Random();
+        return null;
+    }
+
+    @Override
+    public void print(List<Integer> numbers) {
+
+    }
+}
